485b7d83edcee4446dd2b1068fa6eac4c282dfa9
chandrashekharo <chandrashekhar.reddy@anheuser-busch.com>
2024-04-23 13:42:09 +0530

    changed directory

--- Dev/src/product-a/dev/rg/main.tf
+++
@@ -1,7 +0,0 @@
-locals {
-  rg_name = "dev_rg"
-}
-
-output "rgname" {
-  value = local.rg_name
-}
--- Dev/src/product-a/prod/rg/main.tf
+++
@@ -1,7 +0,0 @@
-locals {
-  rg_name = "prod_rg"
-}
-
-output "rgname" {
-  value = local.rg_name
-}
